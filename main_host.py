a0467597cc7bb300b74fabeaf2699fa3e30c5db5
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2025-07-16 15:40:19 -0300

    apply move

--- main_host.py
+++ main_host.py
@@ -1,6 +1,8 @@
 import  socket
 import threading 
 
+SIZE = 3
+
 class JogoDaVelha:
 
     
@@ -36,7 +38,6 @@ class JogoDaVelha:
         return (self.board[int(move[0])][int(move[1])]==" ")
 
 
-
     def connect_opponent(self, host, port): 
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect = ((host, port))
@@ -49,33 +50,66 @@ class JogoDaVelha:
         while self.winner == None:
             if self.moves < 9: 
                 if self.turn == self.me:
-                    move = input("Onde deseja colocar o seu %s no tabuleiro (linha, coluna)?", self.me)
-                    if not self.valid_move(move):
+                    move = input("Onde deseja colocar o seu '%s' no tabuleiro (linha, coluna)?", self.me)
+                    if self.valid_move(move):
                         client.send(move.encode('utf-8'))
                         self.apply_move(move.split(','),self.me)
                         self.turn = self.opponent
-                else:
-                    print("Jogada inválida")
+                    else:
+                        print("Jogada inválida")
                 
-            else:
-                data = client.recv(1024)
-                if not data:
-                    break
                 else:
-                    self.apply_move(data.decode('utf-8').split(','), self.opponent)
-                    self.turn = self.me
+                    data = client.recv(1024)
+                    if not data:
+                        break
+                    else:
+                        self.apply_move(data.decode('utf-8').split(','), self.opponent)
+                        self.turn = self.me
 
-                    
+            else:
+                break
+
         client.close()
     
     def print_board(self):
-        for row in range(3):
+        for row in range(SIZE):
             print(" | ".join(self.board[row]))
-            if row != 2:
+            if row != SIZE - 1:
                 print("----------")
     
     def check_for_winner(self):
-        #Verificar se ganhou na linha
-        #Verificar se ganhou na coluna
-        #Verificar se ganhou na diagonal 1
-        #Verificar se ganhou na diagonal 2
+        #Verifica nas linhas
+        for row in range(SIZE):
+            if self.board[row][0] == self.board[row][1] == self.board[row][2]:
+                self.winner = self.board[row][0]
+                # return
+        
+        #Verifica nas colunas
+        for col in range(SIZE):
+            if self.board[0][col] == self.board[1][col] == self.board[2][col]:
+                self.winner = self.board[0][col]
+                # return
+
+        #Verifica na diagonal 1
+        if self.board[0][0] == self.board[1][1] == self.board[2][2]:
+            self.winner = self.board[0][0]
+            
+        #Verifica na diagonal 2
+        elif self.board[2][0] == self.board[1][1] == self.board[0][2]:
+            self.winner = self.board[2][0]
+        
+    def apply_move(self, move, player):
+        self.moves += 1
+        self.board[int(move[0])][int(move[1])] = player
+        
+        self.print_board()
+        self.check_for_winner()
+        
+        if self.winner == self.me:
+            print("Parabéns, você venceu :)")
+
+        elif self.winner == self.opponent:
+            print("Que pena, você perdeu :(")
+        
+        elif self.moves == 9:
+            print("Deu empate!")
